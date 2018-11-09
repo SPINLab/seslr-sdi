e91fe676905b1a3683e5ed83e168a71718b23daa
Chris Lucas <chris.lucas@geodan.nl>
2018-11-09 15:59:09 +0100

    Period definitions in separate file

---
+++ viewer/js/periods.js
@@ -0,0 +1,315 @@
+const periods = {
+    neolithic: [
+        {
+            text: 'Neolithic',
+            children: [
+                {
+                    text: 'Late Neolithic'
+                },
+                {
+                    text: 'Final Neolithic'
+                },
+                {
+                    text: 'Neolithic'
+                },
+                {
+                    text: 'Uncertain Final Neolithic'
+                },
+                {
+                    text: 'Uncertain Neolithic'
+                }
+            ]
+        }
+    ],
+    bronzeAge: [
+        {
+            text: 'Bronze Age',
+            children: [
+                {
+                    text: 'Early Bronze Age'
+                },
+                {
+                    text: 'Middle Bronze Age'
+                },
+                {
+                    text: 'Late Bronze Age'
+                },
+                {
+                    text: 'Uncertain Early Bronze Age'
+                },
+                {
+                    text: 'Uncertain Middle Bronze Age'
+                },
+                {
+                    text: 'Uncertain Late Bronze Age'
+                }
+            ]
+        }
+    ],
+    protoGeometric: [
+        {
+            text: 'Proto Geometric',
+            children: [
+                {
+                    text: 'Early Proto Geometric'
+                },
+                {
+                    text: 'Middle Proto Geometric'
+                },
+                {
+                    text: 'Late Proto Geometric'
+                },
+                {
+                    text: 'Proto Geometric'
+                },
+                {
+                    text: 'Uncertain Early Proto Geometric'
+                },
+                {
+                    text: 'Uncertain Middle Proto Geometric'
+                },
+                {
+                    text: 'Uncertain Late Proto Geometric'
+                },
+                {
+                    text: 'Uncertain Proto Geometric'
+                }
+            ]
+        }
+    ],
+    geometric: [
+        {
+            text: 'Geometric',
+            children: [
+                {
+                    text: 'Early Geometric'
+                },
+                {
+                    text: 'Middle Geometric'
+                },
+                {
+                    text: 'Late Geometric'
+                },
+                {
+                    text: 'Geometric'
+                },
+                {
+                    text: 'Uncertain Early Geometric'
+                },
+                {
+                    text: 'Uncertain Middle Geometric'
+                },
+                {
+                    text: 'Uncertain Late Geometric'
+                },
+                {
+                    text: 'Uncertain Geometric'
+                }
+            ]
+        }
+    ],
+    archaic: [
+        {
+            text: 'Archaic',
+            children: [
+                {
+                    text: 'Early Archaic'
+                },
+                {
+                    text: 'Middle Archaic'
+                },
+                {
+                    text: 'Late Archaic'
+                },
+                {
+                    text: 'Archaic'
+                },
+                {
+                    text: 'Uncertain Early Archaic'
+                },
+                {
+                    text: 'Uncertain Middle Archaic'
+                },
+                {
+                    text: 'Uncertain Late Archaic'
+                },
+                {
+                    text: 'Uncertain Archaic'
+                }
+            ]
+        }
+    ],
+    classical: [
+        {
+            text: 'Classical',
+            children: [
+                {
+                    text: 'Early Classical'
+                },
+                {
+                    text: 'Middle Classical'
+                },
+                {
+                    text: 'Late Classical'
+                },
+                {
+                    text: 'Classical'
+                },
+                {
+                    text: 'Uncertain Early Classical'
+                },
+                {
+                    text: 'Uncertain Middle Classical'
+                },
+                {
+                    text: 'Uncertain Late Classical'
+                },
+                {
+                    text: 'Uncertain Classical'
+                }
+            ]
+        }
+    ],
+    hellenistic: [
+        {
+            text: 'Hellenistic',
+            children: [
+                {
+                    text: 'Early Hellenistic'
+                },
+                {
+                    text: 'Middle Hellenistic'
+                },
+                {
+                    text: 'Late Hellenistic'
+                },
+                {
+                    text: 'Hellenistic'
+                },
+                {
+                    text: 'Uncertain Early Hellenistic'
+                },
+                {
+                    text: 'Uncertain Middle Hellenistic'
+                },
+                {
+                    text: 'Uncertain Late Hellenistic'
+                },
+                {
+                    text: 'Uncertain Hellenistic'
+                }
+            ]
+        }
+    ],
+    roman: [
+        {
+            text: 'Roman',
+            children: [
+                {
+                    text: 'Early Roman'
+                },
+                {
+                    text: 'Middle Roman'
+                },
+                {
+                    text: 'Late Roman'
+                },
+                {
+                    text: 'Roman'
+                },
+                {
+                    text: 'Uncertain Early Roman'
+                },
+                {
+                    text: 'Uncertain Middle Roman'
+                },
+                {
+                    text: 'Uncertain Late Roman'
+                },
+                {
+                    text: 'Uncertain Roman'
+                }
+            ]
+        }
+    ],
+    byzantine: [
+        {
+            text: 'Byzantine',
+            children: [
+                {
+                    text: 'Early Byzantine'
+                },
+                {
+                    text: 'Middle Byzantine'
+                },
+                {
+                    text: 'Late Byzantine'
+                },
+                {
+                    text: 'Byzantine'
+                },
+                {
+                    text: 'Uncertain Early Byzantine'
+                },
+                {
+                    text: 'Uncertain Middle Byzantine'
+                },
+                {
+                    text: 'Uncertain Late Byzantine'
+                },
+                {
+                    text: 'Uncertain Byzantine'
+                }
+            ]
+        }
+    ],
+    frankish: [
+        {
+            text: 'Frankish',
+            children: [
+                {
+                    text: 'Frankish'
+                },
+                {
+                    text: 'Uncertain Frankish'
+                }
+            ]
+        }
+    ],
+    ottoman: [
+        {
+            text: 'Ottoman',
+            children: [
+                {
+                    text: 'Ottoman'
+                },
+                {
+                    text: 'Uncertain Ottoman'
+                }
+            ]
+        }
+    ],
+    modern: [
+        {
+            text: 'Modern',
+            children: [
+                {
+                    text: 'Modern'
+                },
+                {
+                    text: 'Uncertain Modern'
+                }
+            ]
+        }
+    ],
+    prehistoric: [
+        {
+            text: 'Prehistoric',
+            children: [
+                {
+                    text: 'Uncertain prehistoric date'
+                }
+            ]
+        }
+    ]
+};
