839923afca6b9c997c0c9207542252f99998f63c
Chris Lucas <chris.lucas@geodan.nl>
2018-11-23 11:45:00 +0100

    Add new aerial images

--- viewer/js/CesiumViewer.js
+++ viewer/js/CesiumViewer.js
@@ -260,34 +260,86 @@ for (let featureName of features) {
 
 const imagery = [
     {
-        id: 'WW2',
-        label: 'World War 2',
+        id: '1942',
+        label: '1942',
         children: [
-            { id: 'RLM_13628_sharpened1', label: 'RLM_13628' },
-            { id: 'RLM_13633_sharpened1', label: 'RLM_13633' },
-            { id: 'RLM_13657_detail1_Paleochora1', label: 'RLM_13657' },
-            { id: 'RLM_13728sharpened1', label: 'RLM_13728' },
-            { id: 'RLM_13729sharpened1', label: 'RLM_13729' },
-            { id: 'RLM_13732_sharpened1', label: 'RLM_13732' },
-            { id: 'RLM_13782_11', label: 'RLM_13782' },
-            { id: 'RLM_13632_sharpened1', label: 'RLM_13632' },
-            // { id: 'RLM_13638_detail11', label: 'RLM_13638' },
-            { id: 'RLM_13716_11', label: 'RLM_13716' },
-            // { id: 'RLM_13728sharpened1_reproj', label: 'RLM_13728' },
-            { id: 'RLM_13731sharpened1', label: 'RLM_13731' },
-            { id: 'RLM_13771_enhanced1', label: 'RLM_13771' },
-            { id: 'RLM_13784_11', label: 'RLM_13784' }
+            { id: '1942_13592', label: '13592' },
+            { id: '1942_13594', label: '13594' },
+            { id: '1942_13597', label: '13597' },
+            { id: '1942_13622', label: '13622' },
+            { id: '1942_13625', label: '13625' },
+            { id: '1942_13628', label: '13628' },
+            { id: '1942_13632', label: '13632' },
+            { id: '1942_13633', label: '13633' },
+            { id: '1942_13639', label: '13639' },
+            { id: '1942_13657', label: '13657' },
+            { id: '1942_13719', label: '13719' },
+            { id: '1942_13728', label: '13728' },
+            { id: '1942_13729', label: '13729' },
+            { id: '1942_13730', label: '13730' },
+            { id: '1942_13731', label: '13731' },
+            { id: '1942_13732', label: '13732' },
+            { id: '1942_13770', label: '13770' },
+            { id: '1942_13771', label: '13771' },
+            { id: '1942_13774', label: '13774' },
+            { id: '1942_13780', label: '13780' },
+            { id: '1942_13781', label: '13781' },
+            { id: '1942_13782', label: '13782' },
+            { id: '1942_13784', label: '13784' },
+            { id: '1942_13786', label: '13786' }
         ]
     },
     {
-        id: 'Drone',
-        label: 'Drone',
+        id: '1961',
+        label: '1961',
         children: [
-            { id: 'fs38_kiln_20151106', label: 'Kiln' },
-            { id: 'fs376_EBA_settlement_2015111', label: 'EBA_settlement' },
-            { id: 'fs376_elliniko_20151021', label: 'Elliniko' },
-            { id: 'fs534_quarry_20151012', label: 'Quarry' },
-            { id: 'Karababa_1-2-3-4-5-6-7-8-9', label: 'Karababa' }
+            { id: '1961_14588', label: '14588' },
+            { id: '1961_14590', label: '14590' },
+            { id: '1961_14592', label: '14592' },
+            { id: '1961_14594', label: '14594' },
+            { id: '1961_14596', label: '14596' }
+        ]
+    },
+    {
+        id: '1974',
+        label: '1974',
+        children: [
+            { id: '1974_77495', label: '77495' },
+            { id: '1974_77496', label: '77496' },
+            { id: '1974_77504', label: '77504' },
+            { id: '1974_77506', label: '77506' },
+            { id: '1974_77508', label: '77508' },
+            { id: '1974_77510', label: '77510' },
+            { id: '1974_77512', label: '77512' },
+            { id: '1974_77514', label: '77514' },
+            { id: '1974_77626', label: '77626' },
+            { id: '1974_77627', label: '77627' },
+            { id: '1974_77628', label: '77628' },
+            { id: '1974_77629', label: '77629' },
+            { id: '1974_77630', label: '77630' },
+            { id: '1974_77632', label: '77632' }
+        ]
+    },
+    {
+        id: '1979',
+        label: '1979',
+        children: [
+            { id: '1979_117791', label: '117791' },
+            { id: '1979_117793', label: '117793' },
+            { id: '1979_117951', label: '117951' },
+            { id: '1979_117955', label: '117955' },
+            { id: '1979_117956', label: '117956' },
+            { id: '1979_117958', label: '117958' }
+        ]
+    },
+    {
+        id: '1997',
+        label: '1997',
+        children: [
+            { id: '1997_248568', label: '248568' },
+            { id: '1997_248570', label: '248570' },
+            { id: '1997_248572', label: '248572' },
+            { id: '1997_248574', label: '248574' }
         ]
     }
 ];
