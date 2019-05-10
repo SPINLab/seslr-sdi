17a9ed6b8605778841fd7bcec34d94beed839564
Chris Lucas <chris.lucas@geodan.nl>
2019-05-10 14:40:55 +0200

    Remove unnecessary rewrites for demo mode

--- viewer/set_mode.sh
+++ viewer/set_mode.sh
@@ -1,6 +1,4 @@
 if [ $SESLR_APP_MODE = 'demo' ]; then
-    sed -i 's|../features/find_spots.json|../demo/features/find_spots.json|g' ./src/components/FindSpots/features.js
-    sed -i 's|../features/|../demo/features/|g' ./src/components/Layers/features.js
     sed -i 's|../tms/1.0.0/|../../tms/1.0.0/|g' ./src/components/Layers/imagery.js
     sed -i 's|../tms/1.0.0/|../../tms/1.0.0/|g' ./src/components/Layers/maps.js
     sed -i 's|../terrain/tilesets/tiles|../../terrain/tilesets/tiles|g' ./src/components/Layers/terrain.js
