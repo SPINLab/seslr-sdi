2c3662beaf6bc6e8c3ee61f0427c7f0d7877bcaf
Chris Lucas <chris.lucas@geodan.nl>
2019-05-10 14:23:35 +0200

    Fix small syntax error

--- viewer/set_mode.sh
+++ viewer/set_mode.sh
@@ -1,4 +1,4 @@
-if [ $SESLR_APP_MODE = 'demo']; then
+if [ $SESLR_APP_MODE = 'demo' ]; then
     sed -i 's|../features/find_spots.json|../demo/features/find_spots.json|g' ./src/components/FindSpots/features.js
     sed -i 's|../features/|../demo/features/|g' ./src/components/Layers/features.js
     sed -i 's|../tms/1.0.0/|../../tms/1.0.0/|g' ./src/components/Layers/imagery.js
