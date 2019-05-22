1df82f9c20c8caa8e5be310f1f2d3e4f713133d6
Chris Lucas <chris.lucas@geodan.nl>
2019-05-22 17:15:53 +0200

    Fix aerial photo urls in demo mode

--- viewer/set_mode.sh
+++ viewer/set_mode.sh
@@ -2,4 +2,5 @@ if [ $SESLR_APP_MODE = 'demo' ]; then
     sed -i 's|../tms/1.0.0/|../../tms/1.0.0/|g' ./src/components/Layers/imagery.js
     sed -i 's|../tms/1.0.0/|../../tms/1.0.0/|g' ./src/components/Layers/maps.js
     sed -i 's|../terrain/tilesets/tiles|../../terrain/tilesets/tiles|g' ./src/components/Layers/terrain.js
+    sed -i 's|"../photos/air/${flightPhoto}"|"../../photos/air/${flightPhoto}"|g' ./src/components/Infobox.vue
 fi
